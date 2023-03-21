816427fc16c03d0159cd324967efc833a3ce94a7
Alexs18 <mclexis18@gmail.com>
2023-03-20 21:00:56 -0500

    Init with typescript

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let data = 'date';
+let data2 = 23;
+console.log(data2.toString());
+console.log(data);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let data:string = 'date'
+let data2:number = 23
+
+data = 'Nothing event matter'
+
+console.log(data2.toString());
+console.log(data);
